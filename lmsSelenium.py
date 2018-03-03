48999dd60ecc608b7712183c0dd1d363fd372e9c
yadavsandip32 <yadavsandip32@gmail.com>
2018-03-03 17:28:43 +0530

    Commented some lines

--- lmsSelenium.py
+++ lmsSelenium.py
@@ -17,16 +17,16 @@ window_before = driver.window_handles[0]
 SE = driver.find_elements_by_class_name('launchbutton')
 for count,button in enumerate(SE,1):
     button.click()
-    driver.implicitly_wait(30)
-    window_after = driver.window_handles[1]
-    driver.switch_to.window(window_after)
-    ul = driver.find_element_by_xpath('//*[@id="gridiconcontainer"]/ul')
-
-    links = ul.find_elements_by_xpath('.//li/div/ul/li/p/a/@href')
-
-    for link in links:
-        print(links)
-
+#    driver.implicitly_wait(30)
+#    window_after = driver.window_handles[count]
+#    driver.switch_to.window(window_after)
+#    ul = driver.find_element_by_xpath('//*[@id="gridiconcontainer"]/ul')
+#
+#    links = ul.find_elements_by_xpath('.//li/div/ul/li/p/a/@href')
+#
+#   for link in links:
+#        print(links)
+#
 # window_after = driver.window_handles[1]
 # driver.implicitly_wait(10)
 # links = driver.find_element_by_xpath('//*[@id="module-276925"]/div/div/div[2]/div/a/@href').text
